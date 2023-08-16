cee6d09f342924a7e7fc04635c7045030d5228fd
Hammadjutt124 <hammadjutt7123@gmail.com>
2023-08-16 16:42:23 +0500

    deplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default async function Home() {
     <div>
 
       <Hero />
-      {/*productlist*/}
       <ProductType />
       <ProductCarousel ProductData={response} />
     </div>
--- src/components/views/ProductCarousel/index.tsx
+++ src/components/views/ProductCarousel/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+
 const ProductCarousel = ({ ProductData }: any) => {
 
   return (
