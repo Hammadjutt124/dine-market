1510ee8c5e3af70f35ab74bacc294a201a8af066
Hammadjutt124 <hammadjutt7123@gmail.com>
2023-08-28 00:59:06 +0500

    final

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,68 @@
-'use client';
+
+
+// import Image from "next/image";
+// import { Search, ShoppingCart } from "lucide-react";
+// import {
+//     NavigationMenu,
+//     NavigationMenuContent,
+//     NavigationMenuIndicator,
+//     NavigationMenuItem,
+//     NavigationMenuLink,
+//     NavigationMenuList,
+//     NavigationMenuTrigger,
+//     NavigationMenuViewport,
+// } from "./../ui/navigation-menu";
+
+
+// const Navbar = () => {
+//     return (
+//         <nav className="flex justify-between items-center h-20 px-20">
+//             <Image src={"/Logo.webp"} alt="website logo" width={150} height={150} />
+//             <div>
+//                 <NavigationMenu>
+//                     <NavigationMenuList>
+//                         <NavigationMenuItem className="space-x-5">
+
+//                             <NavigationMenuTrigger>Male</NavigationMenuTrigger>
+
+
+
+
+
+
+//                             <NavigationMenuTrigger>
+//                                 Female
+//                             </NavigationMenuTrigger>
+
+//                             <NavigationMenuTrigger>Kids</NavigationMenuTrigger>
+//                             <NavigationMenuTrigger>All Products</NavigationMenuTrigger>
+//                             <NavigationMenuContent>
+//                                 <NavigationMenuLink></NavigationMenuLink>
+//                             </NavigationMenuContent>
+//                         </NavigationMenuItem>
+//                     </NavigationMenuList>
+//                 </NavigationMenu>
+//             </div>
+//             <div className="flex">
+//                 <Search className="bg-white rounded-l" />{" "}
+//                 <input
+//                     type="text"
+//                     placeholder="What you are looking for"
+//                     className="rounded-r"
+//                 ></input>
+//             </div>
+//             <div className="p-2 rounded-full bg-gray-300">
+//                 <ShoppingCart className="relative" />
+//                 <span className="absolute top-2 right-20 h-6 w-6 text-center rounded-full bg-[#f02d34] text-white">
+//                     0
+//                 </span>
+//             </div>
+//         </nav>
+//     );
+// };
+
+// export default Navbar;
+
 
 
 
@@ -34,12 +98,12 @@ const Header = () => {
                         Kid
                     </Link>
                 </li>
-                <li className='text-lg'>
+                {/* <li className='text-lg'>
 
                     <Link href={"/products"}>
                         All Products
                     </Link>
-                </li>
+                </li> */}
             </ul>
 
             <div className='border flex items-center text-gray-600 px-3 rounded-md'>
