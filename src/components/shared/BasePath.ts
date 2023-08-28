96f75661e0eaf2d981ccf93c5d7020e6f1fd09fd
Hammadjutt124 <hammadjutt7123@gmail.com>
2023-08-28 11:31:01 +0500

    products

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,2 +1,5 @@
-const BASE_PATH_FORAPI = "http://localhost:3000"
+const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
+    "http://localhost:3000"
+    : "https://dine-market-8eyu.vercel.app"
+
 export default BASE_PATH_FORAPI
